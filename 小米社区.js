049f76b010704cbeed004d83e5e879677ad4633e
Liberation <1487098940@qq.com>
2024-03-08 10:20:12 +0800

    更新滑块算法

--- 小米社区.js
+++ 小米社区.js
@@ -18,9 +18,25 @@ function unlock()
         device.wakeUp();
     }
 }
- 
+
 unlock();
 */
+//安卓版本高于Android 9
+////这里的截图有root权限这里不要 start
+if(device.sdkInt>28){
+    //等待截屏权限申请并同意
+    threads.start(function () {
+        packageName('com.android.systemui').text('立即开始').waitFor();
+        text('立即开始').click();
+    });
+}
+//申请截屏权限
+if (!requestScreenCapture()) {
+    toast("请求截图失败");
+    exit()
+}
+////这里的截图有root权限这里不要 end
+
 run();//计时
 //wxts();
 device.wakeUp();//亮屏
@@ -35,14 +51,15 @@ killAPP("小米社区")
 sleep(800);
 主程序1();
 
-//关闭程序
+//关闭程序 这里不同机型的强行停止不一样目前适配vivo 小米
 function killAPP(name) {
     var packageName = app.getPackageName(name)
     app.openAppSetting(packageName)
     sleep(1000)
     while (true) {
-        if (text("结束运行").exists()) {
+        if (textMatches("结束运行|强行停止").exists()) {
             click("结束运行");
+            click("强行停止");
             sleep(1000); 
             while (true) {
                 if (textContains("确定").exists()) {
@@ -61,80 +78,20 @@ function killAPP(name) {
 //打开软件
 function 主程序1() {
     launchApp("小米社区");
-    sleep(1000);
-    className("android.widget.TextView").depth("18").indexInParent("0").findOne().click();
-    sleep(15000);
-    back();
+    log("启动小米社区")
+    sleep(5000);
+    //back();
     className("android.widget.ImageView").depth("12").desc("签到").findOne().click();
+    log("点击签到")
     sleep(10000);
-    className("android.view.View").depth("13").textContains("立即签到").findOne().click();
-    sleep(10000);
-    if (!images.requestScreenCapture()) {
-        log('请求失败');
-        exit();
-    }
-    sleep(2000);
-    var pictures = images.clip(captureScreen(),0,0,device.width,device.height/2);
-    //左上角x y 和宽高
-    images.save(pictures, "/sdcard/Pictures/pictures.png", "png", 100);
-    var img = images.read("/sdcard/Pictures/pictures.png");
-    var g = images.grayscale(img);
-    var result = images.threshold(g, 112, 155);
-    images.save(result, "/sdcard/Pictures/result.png", "png", 100);
-    var image = images.read("/sdcard/Pictures/result.png");
-    var width = image.getWidth();
-    var height = image.getHeight();
-    var path = "/sdcard/Pictures/test.txt";
-    for (let i = 1; i < height; i += 3) {
-        var s = "";
-        for (let j = 1; j < width; j += 3) {
-            var number = images.pixel(image, j, i);
-            var color = colors.toString(number);
-            log(color)
-
-            var ss = color == "#9B9B9B" ? 1 : 0;
-            s += ss;
-        }
-        files.append(path, s + "\n");
-    }
-    var arr = files.read(path).split("\n");
-    var len = -1;
-    for (let i = 0; i < arr.length; i++) {
-        var index = arr[i].indexOf("100000000000000000000000000000000000001");
-        if (index > -1) {
-            len = index * 3 + 60;
-            //60为滑块中心，*3是前面以3为循环
-            //log(len);
-            break;
-        }
-    }
-    if (len > -1) {
-        continuousSwipe(270, 1240, len, 1240, 1000);
-    } else {
-        log("计算失败");
-    }
-    //随机滑动
-    function continuousSwipe(x1, y1, x2, y2, duration) {
-        var steps = 100; // 滑动步数
-        var dx = (x2 - x1) / steps;
-        var dy = (y2 - y1) / steps;
-        var stepDuration = duration / steps; // 每步持续时间
-
-        // 定义手势路径
-        var path = [];
-        for (var i = 0; i <= steps; i++) {
-            var moveX = x1 + dx * i + random(0, 5); // 在x方向加入随机性
-            var moveY = y1 + dy * i + random(-8, 8); // 在y方向加入随机性
-            path.push([moveX, moveY, stepDuration]);
-        }
-
-        // 模拟手势操作
-        gesture.apply(null, [duration].concat(path));
-    }
-    sleep(1000);
-
+    className("android.widget.TextView").depth("13").textContains("立即签到").findOne().click();
+    log("立即签到")
+    sleep(5000);
+    startRec()
     //killAPP("小米社区");
 };
+
+
 
 function run() { //声明运行函数
     threads.start(function () {
@@ -157,4 +114,97 @@ function runTime() {
         launchApp("AutoJs6");
         exit();
     }
+}
+
+var startPosX = 305; //滑动拖拽的中点坐标
+var startPosY = 1829;//滑动拖拽的中点坐标
+var blockWidth = 160;//滑块的宽度
+var verImgLeftTop = [195,1065] //验证码图片部分的左上角坐标
+var verImgRightBottom = [1227,1698] //验证码图片部分的右下角坐标
+/**
+ * 开始识别
+ */
+function startRec() {
+    //这里的截图有root的做好换成
+    //var img = getScreenImage()
+    var img = captureScreen()
+    if (img) {
+        log("截图成功，进行识别滑块！");
+    } else {
+        log('截图失败，重新截图');
+        return;
+    };
+
+    var x = getPointX(img, 0.7);
+    console.info("识别结果滑块X坐标：" + x);
+    swipe(startPosX, startPosY, x + blockWidth/2, startPosY, 1500);
+    sleep(5000);
+}
+
+
+/**
+ * 获取缺口位置的x坐标
+ * 传入值 img, 识别精度(precision)
+ */
+function getPointX(img, precision) {
+    var xCount = 0; 
+    var finnalX = 0;
+    for (var x = verImgLeftTop[0]; x < verImgRightBottom[0]; x += 5) {    //横向遍历像素点，间隔5个像素点
+        // var row = "";
+        var tempCount = 0
+        for (var y = verImgLeftTop[1]; y < verImgRightBottom[1]; y += 5) {      //找到黑点最多的y轴
+            if (isBlackPoint(x, y, img, precision)) {
+                // row +="1";
+                tempCount += 1;
+            } else {
+                // row += "0";
+            }
+        }
+        if (tempCount >= xCount) {
+            xCount = tempCount;
+            finnalX = x
+        }
+        // console.log(row);
+    }
+    return finnalX
+}
+
+/**
+ * 判断点是否为黑色点
+ * 传入值 坐标(x,y), img, 识别精度(precision)
+ */
+function isBlackPoint(x, y, img, precision) {
+    var rgb = images.pixel(img, x, y);    //此时获取到的是ARGB
+    var r = (rgb & 0xff0000) >> 16;      //得到R
+    var g = (rgb & 0xff00) >> 8;            //得到G
+    var b = (rgb & 0xff);                        //得到B
+    var criticalValue = 255 * (1 - precision);
+    if (r <= criticalValue && g <= criticalValue && b <= criticalValue) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * 判断点是否为白色点
+ * 传入值 坐标(x,y), img, 识别精度(precision)
+ */
+function isWhitePoint(x, y, img, precision) {
+    var rgb = images.pixel(img, x, y);  //此时获取到的是ARGB
+    var r = (rgb & 0xff0000) >> 16;   //得到R
+    var g = (rgb & 0xff00) >> 8;        //得到G
+    var b = (rgb & 0xff);                    //得到B
+    var criticalValue = 255 * precision;
+    if (r >= criticalValue && g >= criticalValue && b >= criticalValue) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * 使用命令截图，返回imgae对像。
+ */
+function getScreenImage(){
+    shell("screencap /sdcard/tt_screen_cap1.png",true);
+    return images.read("/sdcard/tt_screen_cap1.png");
 }
