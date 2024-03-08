47ee8a6531a755ebc7ade791852d56efce122549
Liberation <1487098940@qq.com>
2024-03-08 10:39:14 +0800

    增加自动浏览帖子
    代码来自
    https://github.com/2-3-5-7/MiCommunitySemiAutoCheckIn

--- 小米社区.js
+++ 小米社区.js
@@ -23,6 +23,7 @@ unlock();
 */
 //安卓版本高于Android 9
 ////这里的截图有root权限这里不要 start
+const package = "com.xiaomi.vipaccount";
 if(device.sdkInt>28){
     //等待截屏权限申请并同意
     threads.start(function () {
@@ -47,8 +48,7 @@ curTime = new Date();
 date = curTime.getFullYear() + "-" + 0 + (curTime.getMonth() + 1) + "-" + curTime.getDate();
 //获取当前日期
 sleep(500);
-killAPP("小米社区")
-sleep(800);
+浏览帖子()
 主程序1();
 
 //关闭程序 这里不同机型的强行停止不一样目前适配vivo 小米
@@ -75,15 +75,34 @@ function killAPP(name) {
     back();
 }
 
+function 浏览帖子(){
+    do {
+        killAPP("小米社区")
+        sleep(1000);
+
+        launchApp("小米社区");
+        toastLog("等待广告");
+        sleep(6000);
+        toastLog("开始操作");
+    } while (viewPost() === false);
+}
+
 //打开软件
 function 主程序1() {
+    killAPP("小米社区")
+    sleep(800);
     launchApp("小米社区");
     log("启动小米社区")
-    sleep(5000);
+    sleep(6000);
     //back();
     className("android.widget.ImageView").depth("12").desc("签到").findOne().click();
     log("点击签到")
     sleep(10000);
+    if (text("已签到").exists()) {
+        toastLog("已签到，不再签到");
+        killAPP("小米社区")
+        return
+    }
     className("android.widget.TextView").depth("13").textContains("立即签到").findOne().click();
     log("立即签到")
     sleep(5000);
@@ -207,4 +226,72 @@ function isWhitePoint(x, y, img, precision) {
 function getScreenImage(){
     shell("screencap /sdcard/tt_screen_cap1.png",true);
     return images.read("/sdcard/tt_screen_cap1.png");
+}
+
+function viewPost() {
+    let firstPostText;
+    toastLog("开始查找第 1 个非视频帖子");
+    while (true) {
+        let firstPostChild;
+        let counter;
+        for (counter = 0; counter < 3; counter++) {
+            try {
+                firstPostChild = id(`${package}:id/content_view`)
+                    .depth(15)
+                    .find()
+                    .filter(function(ui) {
+                        return ui.bounds().width() > 0; // 存在相同 id 的情况，所以要排除
+                    })[0]
+                    .child(1) // 屏幕第 1 个帖子 (LinearLayout)
+                    .children();
+                break;
+            } catch (error) {
+                if (id(`${package}:id/single_banner`).exists()) {
+                    id(`${package}:id/close`).findOne().click();
+                    toastLog("已关闭弹窗");
+                }
+                toastLog(error);
+                sleep(2000);
+                if (text("重新加载").exists()) {
+                    click("重新加载");
+                    toastLog("点击 重新加载");
+                    sleep(5000);
+                }
+            }
+        }
+        if (counter == 3) {
+            toastLog("超过最大重试次数，没有找到帖子，结束运行");
+            return false;
+        }
+
+        // 找到第 1 个非视频帖子，视频帖子不计算积分
+        if (
+            firstPostChild.length >= 3 &&
+            firstPostChild[1].className() == "android.widget.TextView" &&
+            firstPostChild[2].className() == "android.view.ViewGroup"
+        ) {
+            firstPostText = firstPostChild[1];
+            break;
+        }
+        toastLog("向下滑动查找非视频帖子");
+        const x = deviceWidth / 2,
+            y = (deviceHeight / 4) * 3;
+        swipe(x, y, x, y - 500, 500);
+    }
+
+    // 点击文字部分进入帖子
+    toastLog("点击进入帖子");
+    toastLog(firstPostText.text());
+    click(firstPostText.bounds().centerX(), firstPostText.bounds().centerY());
+
+    // 浏览和点赞
+    toastLog("开始浏览帖子");
+    sleep(15000);
+    // className("android.widget.Button").textStartsWith("点赞").findOne().click();
+    // sleep(1000);
+    back();
+    sleep(1000);
+    back();
+    toastLog("浏览和点赞结束");
+    return true;
 }
